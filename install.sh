8c195efee37e08f614962c91b7d77f27529a3733
Batuhan Akkurt <bai@baturax.com>
2024-11-05 22:30:08 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "sda or nvme?"
 echo "create 3 partitions: first one is efi, second is swap, third is root"
 echo "dont forget to select correct type. for first one, select fat32. for second, select swap. for third, select linit filesystem"
 read output
-bloat="xfsprogs btrfs-progs ipw2100-firmware ipw2200-firmware zd1211-firmware linux-firmware-amd linux-firmware-broadcom base-container-full"
-needed="libusb usbutils dbus connman acpi acpid cpio libaio device-mapper kpartx dracut linux-firmware-network linux6.11 linux6.11-headers"
+bloat="sudo xfsprogs btrfs-progs ipw2100-firmware ipw2200-firmware zd1211-firmware linux-firmware-amd linux-firmware-broadcom base-container-full"
+needed="libusb usbutils dbus connman acpi acpid cpio libaio device-mapper kpartx dracut linux-firmware-network linux6.11 linux6.11-headers "
 installcommand="chroot /mnt /bin/sh -c"
 FSTAB_FILE="/etc/fstab"
 
@@ -63,48 +63,48 @@ fornvme() {
 }
 
 forsda() {
-    ## get disk name
+    ## get disk name    #working
     echo "dont mind this"
     device="/dev/sda"
     umount -R /mnt/
     umount $device*
     swapoff $device*
     rm -rf /mnt/*
-    #cfdisk $device
+    cfdisk $device
     
-    ## format disk
+    ## format disk      #working
     echo "formatting disk"
-    #mkfs.vfat -F32 ${device}1
-    #mkswap ${device}2
-    #mkfs.ext4 ${device}3
+    mkfs.vfat -F32 ${device}1
+    mkswap ${device}2
+    mkfs.ext4 ${device}3
 
-    ## mount disk
+    ## mount disk       #working
     echo "mounting disk"
     mount ${device}3 /mnt
     mkdir -p /mnt/boot/efi
     mount ${device}1 /mnt/boot/efi
     swapon ${device}2
 
-    ## download tarball
-    #downloadtarball
+    ## download tarball     #working
+    downloadtarball
 
-    ## enter chroot
+    ## mount to chroot     #working
     mountfilesandchroot
 
-    ##setup repo
-    #setuprepo
+    ##setup repo            #working
+    setuprepo
 
-    ## install system
-    #installsystem
+    ## install system       #working
+    installsystem
 
-    ## prepare system
-    #prepare
+    ## prepare system       #working
+    prepare
 
-    #setup users
-    #setupusers
+    #setup users        #working
+    setupusers
 
-    ## fstab
-    #bastardfstabsda
+    ## fstab           #working
+    bastardfstabsda
 
     ##install grub
     installgrub
@@ -115,14 +115,71 @@ forsda() {
    chroot /mnt /bin/bash
 }
 
-lasttouch() {
-    $installcommand "xbps-reconfigure -fa"
+
+downloadtarball() {
+    echo "downloading tarball"
+    wget -O /tmp/void.tar.xz https://repo-fastly.voidlinux.org/live/current/void-x86_64-ROOTFS-20240314.tar.xz
+    tar -xvf /tmp/void.tar.xz -C /mnt
 }
+
+mountfilesandchroot() {
+    echo "mounting"
+    mount -t proc none /mnt/proc
+    mount -t sysfs none /mnt/sys
+    mount --rbind /dev /mnt/dev
+    mount --rbind /run /mnt/run
+}
+
+setuprepo() {
+    echo "fastest repos installing"
+    rm /mnt/usr/share/xbps.d/00-repository-main.conf
+    touch /mnt/usr/share/xbps.d/00-repository.conf
+    # Append new repository URLs using echo
+    echo 'repository=https://repo-fastly.voidlinux.org/current' >> /mnt/usr/share/xbps.d/00-repository-main.conf
+    echo 'repository=https://repo-fastly.voidlinux.org/current/nonfree' >> /mnt/usr/share/xbps.d/00-repository-main.conf
+    echo 'repository=https://repo-fastly.voidlinux.org/current/multilib' >> /mnt/usr/share/xbps.d/00-repository-main.conf
+}
+
+installsystem() {
+    sed -i '$a'
+    echo "installing system"
+    $installcommand "xbps-install -Su xbps"
+    $installcommand "xbps-install -u"
+    $installcommand "xbps-install $needed"
+    $installcommand "xbps-remove $bloat"
+}
+
+prepare() {
+    echo "preparing system, better get ready"
+    $installcommand "mount -t efivarfs none /sys/firmware/efi/efivars"
+    vi /mnt/etc/hostname
+    vi /mnt/etc/rc.conf
+    vi /mnt/etc/default/libc-locales
+    $installcommand "xbps-reconfigure -f glibc-locales"
+}
+
+
+setupusers() {
+    echo "enter root password 2 times"
+    $installcommand "passwd root"
+    echo "enter username"
+    read username
+    $installcommand "useradd -m -G wheel,video,audio $username"
+    echo "enter password for $username"
+    $installcommand "passwd $username"
+}
+
 
 installgrub() {
     $installcommand "xbps-install grub-x86_64-efi"
     $installcommand "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Im into VOID"
 }
+
+lasttouch() {
+    $installcommand "xbps-reconfigure -fa"
+}
+
+
 
 bastardfstabsda() {
     $installcommand "rm /etc/fstab"
@@ -151,58 +208,9 @@ swap_UUID=$(chroot /mnt /bin/sh -c "blkid /dev/sda2 | awk -F 'UUID=\"' '{print \
     $installcommand "echo \"tmpfs /tmp tmpfs defaults 0 0\" | tee -a $FSTAB_FILE"
 }
 
-setupusers() {
-    echo "enter root password 2 times"
-    $installcommand "passwd root"
-    echo "enter username"
-    read username
-    $installcommand "useradd -m -G wheel,video,audio $username"
-    echo "enter password for $username"
-    $installcommand "passwd $username"
-}
-
-downloadtarball() {
-    echo "downloading tarball"
-    wget -O /tmp/void.tar.xz https://repo-fastly.voidlinux.org/live/current/void-x86_64-ROOTFS-20240314.tar.xz
-    tar -xvf /tmp/void.tar.xz -C /mnt
-}
-
-mountfilesandchroot() {
-    echo "mounting"
-    mount -t proc none /mnt/proc
-    mount -t sysfs none /mnt/sys
-    mount --rbind /dev /mnt/dev
-    mount --rbind /run /mnt/run
-}
-
-setuprepo() {
-    echo "fastest repos installing"
-    rm /mnt/usr/share/xbps.d/00-repository-main.conf
-    touch /mnt/usr/share/xbps.d/00-repository.conf
-    # Append new repository URLs using echo
-    echo 'repository=https://repo-fastly.voidlinux.org/current' >> /mnt/usr/share/xbps.d/00-repository-main.conf
-    echo 'repository=https://repo-fastly.voidlinux.org/current/nonfree' >> /mnt/usr/share/xbps.d/00-repository-main.conf
-    echo 'repository=https://repo-fastly.voidlinux.org/current/multilib' >> /mnt/usr/share/xbps.d/00-repository-main.conf
-}
 
 
-installsystem() {
-    sed -i '$a'
-    echo "installing system"
-    $installcommand "xbps-install -Su xbps"
-    $installcommand "xbps-install -u"
-    $installcommand "xbps-install $needed"
-    $installcommand "xbps-remove $bloat"
-}
 
-prepare() {
-    echo "preparing system, better get ready"
-    $installcommand "mount -t efivarfs none /sys/firmware/efi/efivars"
-    vi /mnt/etc/hostname
-    vi /mnt/etc/rc.conf
-    vi /mnt/etc/default/libc-locales
-    $installcommand "xbps-reconfigure -f glibc-locales"
-}
 
 if [ "$output" == "sda" ]; then
    forsda
